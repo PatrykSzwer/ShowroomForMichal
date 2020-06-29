39fa9dd0a860183ca750055150826d5168b9fe0b
PatrykSzwer <patryk.szwer@gmail.com>
2020-06-29 14:42:15 +0200

    Created project from template,
    added MyFirstClass with some functionality

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            string A;
+        }
+    }
+}
